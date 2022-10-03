e66d5ee0e48f1a157070bf4dc7c50de5e07c74d3
Ieva Blinstrubienė <ievau99@gmail.com>
2022-10-03 23:24:01 +0300

    Add project files.

---
+++ NETLesson06_PiniguKonvertavimas/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* »Užduotis: Konvertuoti EUR į USD, PLN, BYR
+ * »Naudoti 2 masyvus
+ * »Su valiutų pavadinimais (string[])
+ * »Su valiutų kursu (decimal[]) */
+
+
+string[] valiuta = new string[] { "USD", "PLN", "BYR" };
+decimal[] kursas = new decimal[] { 1.00M, 4.73M, 2.52M };
+
+Console.WriteLine("Koki kieki EUR norite konvertuoti?");
+decimal kiekis = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("I kokia valiuta konvertuosite: [1] USD, [2] PLN, [3] BYR");
+int indeksas = Int32.Parse(Console.ReadLine());
+Console.WriteLine($" jus gausite {Konvertavimas(kursas, kiekis, indeksas)} {valiuta[indeksas -1]}");
+static decimal Konvertavimas(decimal[] kursas, decimal kiekis, int indeksas) => kiekis * kursas[indeksas-1];
